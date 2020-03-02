83fbafa9d2cb197a40e4f7e350ab7ae71d28f20b
DemonPredaqueen <57802854+DemonPredaqueen@users.noreply.github.com>
2020-03-02 11:21:58 -0500

    Update server.tf

--- server.tf
+++ server.tf
@@ -1,6 +1,20 @@
-resource "digitalocean_droplet" "cloudComputer" {
+resource "digitalocean_droplet" "cloudComputer1" {
 	image = "ubuntu-19-10-x64"
-	name = "cloudComputer"
+	name = "cloudComputer1"
 	region = "nyc1"
+	size = "1gb"
+}
+
+resource "digitalocean_droplet" "cloudComputer2" {
+	image = "ubuntu-19-10-x64"
+	name = "cloudComputer2"
+	region = "nyc2"
+	size = "1gb"
+}
+
+resource "digitalocean_droplet" "cloudComputer3" {
+	image = "ubuntu-19-10-x64"
+	name = "cloudComputer3"
+	region = "nyc3"
 	size = "1gb"
 }
